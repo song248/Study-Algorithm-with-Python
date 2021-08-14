c4e601db8f1860e35efde9ef76d04272447a2f2e
song248 <songteagyong@gmail.com>
2021-08-14 12:32:27 +0900

    ch3-2

--- Dongbinna/chapter3. 그리디/큰 수의 법칙.py
+++ Dongbinna/chapter3. 그리디/큰 수의 법칙.py
@@ -19,3 +19,22 @@ while m > 0:
     m -= 1
 
 print(total)
+
+#  ---------------------------------------------
+
+n, m, k = map(int, input().split())
+index = list(map(int, input().split()))
+
+index.sort(reverse=True)
+
+maaaax = index[0]
+neeext = index[1]
+
+count = int(m/(k+1))*k
+count += m%(k+1)
+
+total  = 0
+total += count*maaaax
+total += (m-count)*neeext
+
+print(total)
