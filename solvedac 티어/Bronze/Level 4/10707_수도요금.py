c0463f25a2db6c2ec2f97ba00a5b0d9d1bcbb15c
song248 <songteagyong@gmail.com>
2021-08-17 12:45:00 +0900

    0817_commit_3

---
+++ solvedac 티어/Bronze/Level 4/10707_수도요금.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+if e > c:    b += (e-c)*d
+print(min(a*e,b))
---
+++ solvedac 티어/Bronze/Level 4/5596_시험 점수.py
@@ -0,0 +1,4 @@
+minguk = list(map(int, input().split()))
+manse = list(map(int, input().split()))
+
+print(max(sum(minguk), sum(manse)))
