3a0142605dd7447ce55579d779c89c20d07ec606
song248 <songteagyong@gmail.com>
2021-11-01 14:51:54 +0900

    1101_commit_1

---
+++ solvedac 티어/Bronze/Level 3/4101_크냐.py
@@ -0,0 +1,7 @@
+while True:
+    a, b = map(int, input().split())
+    if a==b==0:
+        break
+    else:
+        if a>b:    print('Yes')
+        else:    print('No')
---
+++ solvedac 티어/Bronze/Level 3/5063_TGN.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    r, e, c = map(int, input().split())
+    if r < e-c:    print('advertise')
+    elif r == e-c:    print('does not matter')
+    else:    print('do not advertise')
