1740173a90f8a8e246b440fe40c61c3ee1f6fe1a
song248 <songteagyong@gmail.com>
2021-12-02 11:17:11 +0900

    1202_commit_2

---
+++ solvedac 티어/Bronze/Level 3/1964_오각형,오각형.py
@@ -0,0 +1,6 @@
+dot = 5
+olim = 7
+for i in range(1, int(input())):
+    dot += olim
+    olim += 3
+print(dot%45678)
---
+++ solvedac 티어/Bronze/Level 3/4504_배수 찾기.py
@@ -0,0 +1,9 @@
+n = int(input())
+while True:
+    a = int(input())
+    if a == 0:
+        break
+    if a%n == 0:
+        print("{} is a multiple of {}.".format(a, n))
+    else:
+        print("{} is NOT a multiple of {}.".format(a, n))
